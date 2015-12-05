cd27cf783d884a6714f9ce0a43b48a1e69205387
Fotomac <ryanasaurus0077@gmail.com>
2015-12-04 22:07:27 -0500

    Update to reflect animated adaptations

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -6,7 +6,7 @@ Route1Mons:
 		db 2,RATTATA
 		db 3,RATTATA
 		db 2,PIDGEY
-		db 3,PIDGEY
+		db 3,SPEAROW
 		db 5,PIDGEY
 		db 4,RATTATA
 		db 6,PIDGEY
@@ -14,11 +14,11 @@ Route1Mons:
 	ELSE
 		db 3,PIDGEY
 		db 3,RATTATA
-		db 3,RATTATA
+		db 3,SPEAROW
 		db 2,RATTATA
 		db 2,PIDGEY
-		db 3,PIDGEY
-		db 3,PIDGEY
+		db 4,SPEAROW
+		db 3,CATERPIE
 		db 4,RATTATA
 		db 4,PIDGEY
 		db 5,PIDGEY
