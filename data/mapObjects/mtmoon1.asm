d2a1c36b2eaa7560a49b9b64fed4ab48066c94a4
Fotomac <ryanasaurus0077@gmail.com>
2016-02-10 17:04:31 -0500

    Update mtmoon1.asm

--- data/mapObjects/mtmoon1.asm
+++ data/mapObjects/mtmoon1.asm
@@ -9,7 +9,7 @@ MtMoon1Object: ; 0x49b06 (size=145)
 	db $f, $19, $3, MT_MOON_2
 
 	db $1 ; signs
-	db $17, $f, $e ; MtMoon1Text14
+	db $17, $f, $f ; MtMoon1Text14
 
 	db $d ; objects
 	object SPRITE_HIKER, $5, $6, STAY, DOWN, $1, OPP_HIKER, $1
@@ -19,12 +19,13 @@ MtMoon1Object: ; 0x49b06 (size=145)
 	object SPRITE_LASS, $10, $17, STAY, DOWN, $5, OPP_LASS, $6
 	object SPRITE_BUG_CATCHER, $7, $16, STAY, DOWN, $6, OPP_BUG_CATCHER, $7
 	object SPRITE_BUG_CATCHER, $1e, $1b, STAY, RIGHT, $7, OPP_BUG_CATCHER, $8
-	object SPRITE_BALL, $2, $14, STAY, NONE, $8, POTION
-	object SPRITE_BALL, $2, $2, STAY, NONE, $9, MOON_STONE
-	object SPRITE_BALL, $23, $1f, STAY, NONE, $a, RARE_CANDY
-	object SPRITE_BALL, $24, $17, STAY, NONE, $b, ESCAPE_ROPE
-	object SPRITE_BALL, $14, $21, STAY, NONE, $c, POTION
-	object SPRITE_BALL, $5, $20, STAY, NONE, $d, TM_12
+	object SPRITE_BLACK_HAIR_BOY_2, $1f, $10, STAY, RIGHT, $8, OPP_BROCK, $2
+	object SPRITE_BALL, $2, $14, STAY, NONE, $9, POTION
+	object SPRITE_BALL, $2, $2, STAY, NONE, $a, MOON_STONE
+	object SPRITE_BALL, $23, $1f, STAY, NONE, $b, RARE_CANDY
+	object SPRITE_BALL, $24, $17, STAY, NONE, $c, ESCAPE_ROPE
+	object SPRITE_BALL, $14, $21, STAY, NONE, $d, POTION
+	object SPRITE_BALL, $5, $20, STAY, NONE, $e, TM_12
 
 	; warp-to
 	EVENT_DISP MT_MOON_1_WIDTH, $23, $e
