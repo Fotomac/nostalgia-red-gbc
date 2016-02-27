ee6343040de7f29f0a6ca173fd642aaf3bcd790d
Fotomac <ryanasaurus0077@gmail.com>
2016-02-27 00:35:56 -0500

    Fixing compile error

---
+++ data/special_trainers.asm
@@ -0,0 +1,56 @@
+SpecialTrainerIDs:
+	db SONY1
+	db GIOVANNI
+	db ROCKET
+	db BRUNO
+	db BROCK
+	db MISTY
+	db LT__SURGE
+	db ERIKA
+	db KOGA
+	db BLAINE
+	db SABRINA
+	db SONY2
+	db SONY3
+	db LORELEI
+	db AGATHA
+	db LANCE
+	db "@"
+
+SpecialTrainerIDs2:
+	db SONY1
+	db GIOVANNI
+	db ROCKET
+	db BRUNO
+	db BROCK
+	db MISTY
+	db LT__SURGE
+	db ERIKA
+	db KOGA
+	db BLAINE
+	db SABRINA
+	db SONY2
+	db SONY3
+	db LORELEI
+	db AGATHA
+	db LANCE
+	db "@"
+
+SpecialTrainerIDs3:
+	db SONY1
+	db GIOVANNI
+	db ROCKET
+	db BRUNO
+	db BROCK
+	db MISTY
+	db LT__SURGE
+	db ERIKA
+	db KOGA
+	db BLAINE
+	db SABRINA
+	db SONY2
+	db SONY3
+	db LORELEI
+	db AGATHA
+	db LANCE
+	db "@"
