5d3329d7196618ff9bb07198d2920521f5337533
Fotomac <ryanasaurus0077@gmail.com>
2016-04-02 03:36:27 -0400

    Colorizing

---
+++ color/super_palettes.asm
@@ -0,0 +1,48 @@
+SECTION "bank30",ROMX,BANK[$30]
+LoadSGBPalette:
+	ld a,e
+	ld l,d
+	ld h,0
+	add hl
+	add hl
+	add hl
+	ld de,SuperPalettes
+	add hl,de
+
+	ld de,W2_BgPaletteData
+	jr startPaletteTransfer
+
+LoadSGBPalette_Sprite:
+	ld a,e
+
+	ld l,d
+	ld h,0
+	add hl
+	add hl
+	add hl
+	ld de,SuperPalettes
+	add hl,de
+
+	ld de,W2_BgPaletteData + $40
+
+startPaletteTransfer:
+	add a
+	add a
+	add a
+	add e
+	ld e,a
+	ld b,8
+	
+.palLoop
+	ld a,[hli]
+	ld [de],a
+	inc de
+	dec b
+	jr nz,.palLoop
+	ret
+
+IF GEN_2_GRAPHICS
+	INCLUDE "color/gen2_palettes.asm"
+ELSE
+	INCLUDE "data/super_palettes.asm"
+ENDC
